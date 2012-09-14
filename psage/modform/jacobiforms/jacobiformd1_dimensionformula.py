5b1834b1b3cc8166712f5838bac9c238a45b8a99
Martin Raum <martin@raum-brothers.eu>
2012-09-14 17:36:54 +0200

    Implement first version of the dimension formula (not yet checked).

---
+++ psage/modform/jacobiforms/jacobiformd1_dimensionformula.py
@@ -0,0 +1,214 @@
+r"""
+A dimension formula for vector-valued modular forms, and functions that
+apply it to the case of Jacobi forms
+"""
+
+#===============================================================================
+# 
+# Copyright (C) 2012 Martin Raum
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
+
+from sage.functions.all import exp, sqrt, sign
+from sage.matrix.all import diagonal_matrix, identity_matrix, matrix
+from sage.misc.all import sum, mrange
+from sage.modules.all import vector 
+from sage.rings.all import ComplexField, ZZ, lcm
+from sage.symbolic.all import I, pi
+from copy import copy
+import mpmath
+import operator
+
+#===============================================================================
+# dimension__vector_valued
+#===============================================================================
+
+def dimension__vector_valued(k, L) :
+    r"""
+    Compute the dimension of the space of weight `k` vector valued modular forms
+    for the Weil representation attached to the lattice `L`.
+    
+    See [Borcherds, Lorenzian ???] for a proof of the formula that we use here.
+    
+    INPUT:
+    
+    - `k` -- A half-integer.
+    
+    - ``L`` -- An quadratic form.
+    
+    OUTPUT:
+        An integer.
+
+    TESTS::
+    
+        sage: ???
+    """
+    if k <= 0 :
+        return 0
+    if k == 1 :
+        raise NotImplementedError( "Weight 1 is not implemented." )
+    k = ZZ(k)
+
+    ## The bilinear and the quadratic form attached to L
+    quadratic = lambda x: L(x) // 2
+    bilinear = lambda x,y: L(x + y) - L(x) - L(y)
+
+    ## A dual basis for L
+    (elementary_divisors, dual_basis_pre, _) = L.matrix().smith_form()
+    elementary_divisors = elementary_divisors.diagonal() 
+    dual_basis = map(operator.div, list(dual_basis_pre), elementary_divisors)
+    
+    L_level = ZZ(lcm([ b.denominator() for b in dual_basis ]))
+    L_dimension = L.matrix().ncols()
+    
+    (elementary_divisors, _, discriminant_basis_pre) = (L_level * matrix(dual_basis)).change_ring(ZZ).smith_form()
+    elementary_divisors = filter( lambda d: d not in ZZ, (elementary_divisors / L_level).diagonal() )
+    elementary_divisors_inv = map(ZZ, [ed**-1 for ed in elementary_divisors])
+    discriminant_basis = matrix(map( operator.mul,
+                                     discriminant_basis_pre.inverse().rows()[:len(elementary_divisors)],
+                                     elementary_divisors )).transpose()
+    ## This is a form over QQ, so that we cannot use an instance of QuadraticForm
+    discriminant_form = discriminant_basis.transpose() * L.matrix() * discriminant_basis
+
+    disc_quadratic = lambda x : -x * discriminant_form * x / 2
+    disc_bilinear = lambda x,y : disc_quadratic(x + y) - disc_quadratic(x) - disc_quadratic(y)
+
+    ## red gives a normal form for elements in the discriminant group
+    red = lambda x : vector(map(operator.mod, x, elementary_divisors_inv))
+    ## singls and pairs are elements of the discriminant group that are, respectively,
+    ## fixed and not fixed by negation. 
+    singls = filter(lambda x: red(-x) - x == 0, mrange(elementary_divisors_inv, vector))
+    pairs = filter(lambda x: red(-x) - x != 0 and x < red(-x), mrange(elementary_divisors_inv, vector))
+
+    if k % 2 == 0 :
+        subspace_dimension = len(singls + pairs)
+    else :
+        subspace_dimension = len(pairs)
+
+    ## 20 bits are, by far, sufficient to distinguish 12-th roots of unity
+    ## by increasing the precision by 4 for each additional dimension, we
+    ## compensate, by far, the errors introduced by the QR decomposition,
+    ## which are of the size of (absolute error) * dimension
+    CC = ComplexField(20 + subspace_dimension * 4)
+
+    zeta_order = ZZ(lcm([8] + elementary_divisors_inv))
+
+    zeta = CC(exp(2 * pi * I / zeta_order))
+    sqrt2  = CC(sqrt(2))
+    drt  = CC(sqrt(L.det()))
+
+    print [disc_bilinear(gamma, delta) for gamma in pairs for delta in pairs]
+
+    Tmat  = diagonal_matrix(CC, [zeta**(zeta_order*disc_quadratic(a)) for a in (singls + pairs if k % 2 == 0 else pairs)])
+    if k % 2 == 0 :
+#        Smat = zeta**(zeta_order / 8 * L_dimension) / drt  \
+#               * matrix( CC, [  [zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in singls]
+#                              + [zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in pairs]
+#                              + [zeta**(-zeta_order * disc_bilinear(gamma,-delta)) for delta in pairs]
+#                              for gamma in singls] \
+#                           + [  [zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in singls]
+#                              + [zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in pairs]
+#                              + [zeta**(-zeta_order * disc_bilinear(gamma,-delta)) for delta in pairs]
+#                              for gamma in pairs] \
+#                           + [  [zeta**(-zeta_order * disc_bilinear(-gamma,delta)) for delta in singls]
+#                              + [zeta**(-zeta_order * disc_bilinear(-gamma,delta)) for delta in pairs]
+#                              + [zeta**(-zeta_order * disc_bilinear(-gamma,-delta)) for delta in pairs]
+#                              for gamma in pairs] )
+        
+        Smat = zeta**(zeta_order / 8 * L_dimension) / drt  \
+               * matrix( CC, [  [zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in singls]
+                              + [sqrt2 * zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in pairs]
+                              for gamma in singls] \
+                           + [  [sqrt2 * zeta**(-zeta_order * disc_bilinear(gamma,delta)) for delta in singls]
+                              + [zeta**(-zeta_order * disc_bilinear(gamma,delta)) + zeta**(-zeta_order * disc_bilinear(gamma,-delta)) for delta in pairs]
+                              for gamma in pairs] )
+    else :
+        Smat = zeta**(zeta_order / 8 * L_dimension) / drt  \
+               * matrix( CC, [  [zeta**(-zeta_order * disc_bilinear(gamma,delta)) - zeta**(-zeta_order * disc_bilinear(gamma,-delta))  for delta in pairs]
+                               for gamma in pairs ] )
+    STmat = Smat * Tmat
+    
+    return Smat
+    
+#    print Smat**8
+#    print
+#    print
+    
+    def eigenvalue_multiplicity(mat, ev) :
+        #TODO: Adjust
+        eps = 10**-10
+#        print mat.minpoly().factor()
+#        print
+#        print
+        mat = matrix(CC, mat - ev * identity_matrix(subspace_dimension))
+        print mat.minpoly()
+        print _qr(mat)
+        print
+        return len(filter( lambda row: all( abs(e) < eps for e in row), _qr(mat) ))
+    
+    i = CC(exp(2 * pi * I / 8))
+    S_ev_multiplicity = [eigenvalue_multiplicity(Smat, i**n) for n in range(8)]
+    print S_ev_multiplicity, subspace_dimension
+    assert sum(S_ev_multiplicity) == subspace_dimension
+
+    rho = CC(exp(2 * pi * I / 12))
+    ST_ev_multiplicity = [eigenvalue_multiplicity(STmat, rho**n) for n in range(12)]
+    assert sum(ST_ev_multiplicity) == subspace_dimension
+
+    T_evs = [ ZZ((-zeta_order * disc_quadratic(a)) % zeta_order) / zeta_order
+              for a in singls + pairs ]
+
+    return subspace_dimension * (1 + k / 12) \
+           - ZZ(sum( (S_ev_multiplicity * ((k - n) % 8)) for n in range(8) )) / 8 \
+           - ZZ(sum( (ST_ev_multiplicity * ((-2*k + n) % 12)) for n in range(12) )) / 12 \
+           - sum(T_evs)
+
+def _qr(mat, eps = 10**-10) :
+    r"""
+    Compute the R matrix in QR decomposition using Housholder reflections.
+    
+    This is an adoption of the implementation in mpmath by Andreas Strombergson. 
+    """
+    CC = mat.base_ring()
+    mat = copy(mat)
+    m = mat.nrows()
+    n = mat.ncols()
+
+    p = []
+    cur_row = 0
+    for j in range(0, n) :
+        s = sum( (abs(mat[i,j]))**2 for i in xrange(cur_row, m) )
+        if abs(s) <= eps :
+            for i in range(cur_row, m) :
+                mat[i,j] = CC(0)
+            continue
+            
+        p = sqrt(s)
+        kappa = 1 / (s - p * mat[cur_row,j])
+
+        mat[cur_row,j] -= p
+        for k in range(j + 1, n) :
+            y = sum(mat[i,j].conjugate() * mat[i,k] for i in xrange(cur_row, m)) * kappa
+            for i in range(cur_row, m):
+                mat[i,k] -= mat[i,j] * y
+
+        mat[cur_row,j] = p
+        for i in range(cur_row + 1, m) :
+            mat[i,j] = CC(0)
+        
+        cur_row += 1
+    
+    return mat
