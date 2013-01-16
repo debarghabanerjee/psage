7ff559ff36812061730aef679990c62d773fb122
Martin Raum <martin@raum-brothers.eu>
2013-01-16 10:00:02 +0100

    Add licences to lattce short vector files.

--- psage/lattice/precomputed_short_vectors/precompute_short_vectors.py
+++ psage/lattice/precomputed_short_vectors/precompute_short_vectors.py
@@ -1,7 +1,27 @@
+#===============================================================================
+# 
+# Copyright (C) 2012 Martin Raum
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
+
+import os
 from sage.interfaces.magma import magma
 from sage.rings.all import Integer
 from short_vector_file__python import ShortVectorFile__python
-import os
+
 
 def precompute_short_vectors__magma( lattice, lengths, output_file_name, maximal_length = None ) :
     r"""
